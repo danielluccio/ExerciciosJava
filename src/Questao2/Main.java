8572d8543eddab0961ceda0c55c20dad82e8e4e7
danielluccio <daniel130877@gmail.com>
2025-04-16 12:10:07 -0300

    Questao 2

---
+++ src/Questao2/Main.java
@@ -0,0 +1,9 @@
+package Questao2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(
+                "A herança é um dos pilares da POO, sendo útil no compartilhamento entre classes de atributos e comportamentos");
+        System.out.println("Ela é util pois facilita a reutilização de codigo!");
+    }
+}
