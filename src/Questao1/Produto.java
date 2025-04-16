7105c4d0293c62ad7218d435d1a4a58d1a7272aa
danielluccio <daniel130877@gmail.com>
2025-04-16 12:06:26 -0300

    Classe Produto Finalizada

--- src/Questao1/Produto.java
+++ src/Questao1/Produto.java
@@ -1,5 +1,51 @@
 package Questao1;
 
 public class Produto {
-    // Construtor
+    // Construtor Padrao
+
+    Produto() {
+
+    }
+
+    private String nome;
+    private double valor;
+    private int quantidade;
+
+    Produto(String nome, double valor, int quantidade) {
+        this.nome = nome;
+        this.valor = valor;
+        this.quantidade = quantidade;
+
+    }
+
+    // Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double calcularTotal() {
+        return valor * quantidade;
+    }
+
 }
